585bd1471a66a4bf509293f35de0904a256730c6
bhav <bhavyashah248@gmail.com>
2018-03-20 18:44:52 +0530

    new_models

--- MainBS/admin.py
+++ MainBS/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Question
+from .models import Question, Subject, Type, Chapter
 
 admin.site.register(Question)
-
+admin.site.register(Subject)
+admin.site.register(Type)
+admin.site.register(Chapter)
--- MainBS/models.py
+++ MainBS/models.py
@@ -5,11 +5,37 @@ from django.db import models
 
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=30)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=30)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
-    subject = models.CharField(max_length=30)
-    chapter = models.CharField(max_length=10)
-    type = models.CharField(max_length=10)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     marks = models.CharField(max_length=10)
     question = models.CharField(max_length=200)
     q_id = models.CharField(max_length=20)
     answer = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.q_id
